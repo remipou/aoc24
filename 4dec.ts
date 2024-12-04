9d760c372e55ce875f300114b85b5349a1c7686a
Rémi Puig <remi.puig@gmail.com>
2024-12-04 10:35:22 +0100

    day 4 - part 1

---
+++ 4dec.ts
@@ -0,0 +1,80 @@
+import getData from './data';
+
+const [_ts, _file, date, test] = process.argv;
+const data: string[][] = getData(date, test)
+  .split('\n')
+  .map((l) => l.split(''));
+// console.log(data);
+
+interface Pos {
+  x: number;
+  y: number;
+}
+
+const r1 = /XMAS/g;
+const r2 = /SAMX/g;
+
+const findInString = (str: string): number => {
+  const c1 = [...str.matchAll(r1)].length;
+  const c2 = [...str.matchAll(r2)].length;
+  // console.log(str, c1, c2);
+  return c1 + c2;
+};
+
+const makeString = (start: Pos, reverse: boolean = false): string | null => {
+  let str = data[start.y][start.x];
+  const pos: Pos = { ...start };
+  if (reverse) {
+    while (pos.y < data[0].length - 1 && pos.x > 0) {
+      console.log(pos);
+      pos.x = pos.x - 1;
+      pos.y = pos.y + 1;
+      str += data[pos.y][pos.x];
+    }
+  } else {
+    while (pos.x < data[0].length - 1 && pos.y < data.length - 1) {
+      pos.x = pos.x + 1;
+      pos.y = pos.y + 1;
+      str += data[pos.y][pos.x];
+    }
+  }
+  return str.length > 3 ? str : null;
+};
+
+console.time('part 1');
+
+let count = 0;
+for (let i = 0; i < data.length; i++) {
+  count = count + findInString(data[i].join(''));
+}
+for (let i = 0; i < data[0].length; i++) {
+  count = count + findInString(data.map((l) => l[i]).join(''));
+}
+for (let i = 0; i < data.length; i++) {
+  const str = makeString({ x: 0, y: i });
+  const str3 = i > 0 ? makeString({ x: i, y: 0 }) : null;
+
+  const str2 = makeString({ x: data[0].length - 1, y: i }, true);
+  const str4 =
+    i > 0 ? makeString({ x: data[0].length - 1 - i, y: 0 }, true) : null;
+
+  if (str) {
+    count = count + findInString(str);
+  }
+  if (str2) {
+    count = count + findInString(str2);
+  }
+  if (str3) {
+    count = count + findInString(str3);
+  }
+  if (str4) {
+    count = count + findInString(str4);
+  }
+}
+
+console.timeEnd('part 1');
+console.log('part1', count);
+
+console.time('part 2');
+console.timeEnd('part 2');
+console.log('part2');
